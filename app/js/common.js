e48d998d99c9873f3a9036bb82b8e576ccf1927c
dpodbereznyh <dpodbereznyh@gmail.com>
2020-09-11 16:30:25 +0500

    Final

--- app/js/common.js
+++ app/js/common.js
@@ -120,30 +120,7 @@ function initStuff () {
 
     });
 
-    //Map
-    // $('.faq__item_head').click(function () {
-        myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
-            hintContent: 'Собственный значок метки с контентом',
-            balloonContent: 'А эта — новогодняя',
-            iconContent: '12'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#imageWithContent',
-            // Своё изображение иконки метки.
-            iconImageHref: 'images/ball.png',
-            // Размеры метки.
-            iconImageSize: [48, 48],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-24, -24],
-            // Смещение слоя с содержимым относительно слоя с картинкой.
-            iconContentOffset: [15, 15],
-            // Макет содержимого.
-            iconContentLayout: MyIconContentLayout
-        });
 
-	// }
 
 	$(document).on('click', '.js-scroll-content', function () {
 		var target = document.querySelector('.move');
@@ -168,6 +145,7 @@ function mobileSlider() {
             wrapperClass: 'recommend__wrapper',
             slidesPerView: 1,
             spaceBetween: 32,
+			autoHeight: true,
             navigation: {
                 nextEl: '.recommend__button-next',
                 prevEl: '.recommend__button-prev',
@@ -228,6 +206,7 @@ function initSliders () {
 			slideClass: 'table__slide',
 			wrapperClass: 'table__wrapper',
 			slidesPerView: 1,
+			autoHeight: true,
 			threshold: 4,
 			// controller: {
 			// 	control: dateSlider,
